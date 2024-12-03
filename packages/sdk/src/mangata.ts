41c7bc392a321d0e4f6cb6dcc672bd82e8bb7321
Dominik Fedor <DominikFedor@users.noreply.github.com>
2024-12-03 11:19:22 +0100

    chore: export getPoolRatio

--- packages/sdk/src/mangata.ts
+++ packages/sdk/src/mangata.ts
@@ -2,10 +2,13 @@ import "gasp-types";
 import { PriceImpact } from "./types/utility";
 import { getPriceImpact } from "./utils/getPriceImpact";
 import { createMangataInstance } from "./mangataInstance";
+import { getRatio } from "./utils/getRatio";
+import { BN } from "@polkadot/util";
 
 const Mangata = {
   instance: createMangataInstance,
-  getPriceImpact: (args: PriceImpact) => getPriceImpact(args)
+  getPriceImpact: (args: PriceImpact) => getPriceImpact(args),
+  getPoolRatio: (left: BN, right: BN) => getRatio(left, right)
 };
 
 export { Mangata };
